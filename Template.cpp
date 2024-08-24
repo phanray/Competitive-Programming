94908bccdc6f82243044e226926b93a9ff8701e2
Phan Thanh Đạt <datp3877@gmail.com>
2024-08-24 14:57:43 +0700

    Update and rename 1.cpp to Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -13,12 +13,6 @@ using namespace std;
 template<class A,class B> inline void maximize(A& x, B y) {x = max(x, y);};
 template<class A,class B> inline void minimize(A& x, B y) {x = min(x, y);};
 
-mt19937 rd(chrono::steady_clock::now().time_since_epoch().count());
-
-ll rand(ll a,ll b) {
-     return uniform_int_distribution<int>(a,b)(rd);
-}
-
 const int maxn = 1e6 + 5;
 const ll mod = 1e9 + 7;
 
