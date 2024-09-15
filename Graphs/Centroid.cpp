82170f5cf3026b91c59e3b55da784c59fa2a67d6
Phan Thanh Đạt <datp3877@gmail.com>
2024-09-15 17:49:05 +0700

    Update Centroid.cpp

--- Graphs/Centroid.cpp
+++ Graphs/Centroid.cpp
@@ -1,56 +1,46 @@
 struct Centroid{
+    int child[maxn];
+    bool del[maxn];
 
-    int maxh, cnt[N], del[N], child[N];
+    int findCentroid(int u, int par, int n) {
+        for (int x: e[u])
+            if (!del[x] && x != par && child[x] > n / 2)
+                return findCentroid(x, u, n); 
+
+        return u;
+    }
 
     void countChild(int u, int par) {
-
         child[u] = 1;
-        for (int x : edge[u])
-            if (x != par && !del[x]) {
+
+        for (int x: e[u])
+            if (!del[x] && x != par) {
 
                 countChild(x, u);
                 child[u] += child[x];
             } 
     }
 
-    int centroid(int u, int par, int n) {
+    void dfs(int u, int par) {
 
-        for (int x :edge[u])
-            if (x != par && !del[x] && child[x] > n / 2)
-                return centroid(x, u, n); 
-
-        return u;
-    }
-
-    void dfs(int u, int par, int type) {
-
-        if (type) {
-
-            //...
-        } else {
-
-            //...
-        }
+        for (int x: e[u])
+            if (!del[x] && x != par)
+                dfs(x, u);
     }
 
     void solve(int u) {
-
         countChild(u, -1);
         int n = child[u];
-        int root = centroid(u, -1, n);
+        int root = findCentroid(u, -1, n);
 
-        for (int x :edge[root])
-            if (!del[x]) {
-
-                dfs(x, root, 0);
-                dfs(x, root, 1);
-
-            }
+        for (int x: e[root])
+            if (!del[x]) 
+                dfs(x, root);
 
         del[root] = 1;
-        for (int x :edge[root])
-            if (!del[x]) solve(x);
-
+        for (int x: e[root])
+            if (!del[x]) 
+                solve(x);
     }
 
 } centroid;
