13c2bf80c50ed0729ae195565fd04e27d62492b2
Phan Thanh Đạt <datp3877@gmail.com>
2024-09-15 17:59:41 +0700

    Update WaveletTree.cpp

--- Data-structure/WaveletTree.cpp
+++ Data-structure/WaveletTree.cpp
@@ -1,5 +1,4 @@
 struct WaveletTree {
-
     WaveletTree * childL, * childR;
     vector<int> prefix;
     int l, r;
@@ -11,10 +10,10 @@ struct WaveletTree {
 
     }
 
-    void build(int posL, int posR) {
+    void build(int posL, int posR, vector<int> &a) {
 
-        l = *min_element(a + posL, a + posR + 1);
-        r = *max_element(a + posL, a + posR + 1);
+        l = *min_element(a.begin() + posL, a.begin() + posR + 1);
+        r = *max_element(a.begin() + posL, a.begin() + posR + 1);
 
         if (l == r || posL > posR)
             return;
@@ -25,48 +24,17 @@ struct WaveletTree {
         for (int i = posL; i <= posR; i++)
             prefix.push_back(prefix.back() + (a[i] <= mid));
         
-        int posMid = stable_partition(a + posL, a + posR + 1, [&](int x) {
+        int posMid = stable_partition(a.begin() + posL, a.begin() + posR + 1, [&](int x) {
 
             return x <= mid;
 
-        }) - a - 1;
+        }) - a.begin() - 1;
 
         childL = new WaveletTree;
-        childL->build(posL, posMid);
-
         childR = new WaveletTree;
-        childR->build(posMid + 1, posR);
 
-    }
-
-    int Kth(int posL, int posR, int k) {
-
-        if (l == r)
-            return l;
-        
-        int cntL = prefix[posL - 1];
-        int cntR = prefix[posR];
-        int quantity = cntR - cntL;
-
-        if (quantity >= k)
-            return childL->Kth(cntL + 1, cntR, k);
-        else
-            return childR->Kth(posL - cntL, posR - cntR, k - quantity);
-
-    }
-
-    int cnt(int posL, int posR, int k) {
-
-        if (l > k || posL > posR)
-            return 0;
-
-        if (r <= k)
-            return posR - posL + 1;
-        
-        int cntL = prefix[posL - 1];
-        int cntR = prefix[posR];
-
-        return childL->cnt(cntL + 1, cntR, k) + childR->cnt(posL - cntL, posR - cntR, k);
+        childL->build(posL, posMid, a);
+        childR->build(posMid + 1, posR, a);
 
     }
 
@@ -85,4 +53,34 @@ struct WaveletTree {
 
     }
 
+    int cnt(int posL, int posR, int k) {
+
+        if (r < k || posL > posR)
+            return 0;
+
+        if (l >= k)
+            return posR - posL + 1;
+        
+        int cntL = prefix[posL - 1];
+        int cntR = prefix[posR];
+
+        return childL->cnt(cntL + 1, cntR, k) + childR->cnt(posL - cntL, posR - cntR, k);
+
+    }
+
+    int Kth(int posL, int posR, int k) {
+
+        if (l == r)
+            return l;
+        
+        int cntL = prefix[posL - 1];
+        int cntR = prefix[posR];
+        int quantity = cntR - cntL;
+
+        if (quantity >= k)
+            return childL->Kth(cntL + 1, cntR, k);
+        else
+            return childR->Kth(posL - cntL, posR - cntR, k - quantity);
+
+    }
 } wave;
