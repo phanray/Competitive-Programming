c16a474f87e40be64dd8f996a72af3e2e69661fa
Phan Thanh Đạt <datp3877@gmail.com>
2024-11-19 22:49:08 +0700

    Update FenTree.cpp

--- Data-structure/FenTree.cpp
+++ Data-structure/FenTree.cpp
@@ -1,9 +1,12 @@
 struct BIT{
-
     int ft[maxn];
+    int n;
+
+    void init(int _n){
+        n = _n;
+    }
 
     void update(int u, int v){
-        
         while (u <= n) {
             ft[u] += v;
             u += (u & (-u));
@@ -20,4 +23,7 @@ struct BIT{
         return ans;
     }
 
+    int get(int l, int r){
+        return get(r) - get(l - 1);
+    }
 } fen;
