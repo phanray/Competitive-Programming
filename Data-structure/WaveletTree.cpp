7bdaf7f8d27051d088c950523b5d81862ea201ae
Phan Thanh Đạt <datp3877@gmail.com>
2024-11-29 15:35:01 +0700

    Update WaveletTree.cpp

--- Data-structure/WaveletTree.cpp
+++ Data-structure/WaveletTree.cpp
@@ -1,86 +1,74 @@
 struct WaveletTree {
-    WaveletTree * childL, * childR;
-    vector<int> prefix;
+    WaveletTree *L, *R;
+    vector<int> pre;
     int l, r;
 
-    WaveletTree () {
-
-        childL = childR = NULL;
+    WaveletTree(){
+        L = R = NULL;
         l = r = 0;
-
     }
 
-    void build(int posL, int posR, vector<int> &a) {
-
+    void build(int posL, int posR, vector<int> &a){
         l = *min_element(a.begin() + posL, a.begin() + posR + 1);
         r = *max_element(a.begin() + posL, a.begin() + posR + 1);
 
         if (l == r || posL > posR)
             return;
 
-        prefix.push_back(0);
+        int mid = l + r >> 1;
+        pre.pb(0);
 
-        int mid = (l + r) / 2;
-        for (int i = posL; i <= posR; i++)
-            prefix.push_back(prefix.back() + (a[i] <= mid));
-        
-        int posMid = stable_partition(a.begin() + posL, a.begin() + posR + 1, [&](int x) {
+        for (int i = posL; i <= posR; ++i)
+            pre.pb(pre.back() + (a[i] <= mid));
 
+        int posMid = stable_partition(a.begin() + posL, a.begin() + posR + 1, [&](int x){
             return x <= mid;
-
         }) - a.begin() - 1;
 
-        childL = new WaveletTree;
-        childR = new WaveletTree;
+        L = new WaveletTree;
+        R = new WaveletTree;
 
-        childL->build(posL, posMid, a);
-        childR->build(posMid + 1, posR, a);
-
+        L->build(posL, posMid, a);
+        R->build(posMid + 1, posR, a);
     }
 
     int count(int posL, int posR, int k) {
-
         if (l > k || r < k || posL > posR)
             return 0;
 
         if (l == r)
             return posR - posL + 1;
 
-        int cntL = prefix[posL - 1];
-        int cntR = prefix[posR];
+        int cntL = pre[posL - 1];
+        int cntR = pre[posR];
 
-        return childL->count(cntL + 1, cntR, k) + childR->count(posL - cntL, posR - cntR, k);
-
+        return L->count(cntL + 1, cntR, k) + R->count(posL - cntL, posR - cntR, k);
     }
 
     int cnt(int posL, int posR, int k) {
-
         if (r < k || posL > posR)
             return 0;
 
         if (l >= k)
             return posR - posL + 1;
-        
-        int cntL = prefix[posL - 1];
-        int cntR = prefix[posR];
 
-        return childL->cnt(cntL + 1, cntR, k) + childR->cnt(posL - cntL, posR - cntR, k);
+        int cntL = pre[posL - 1];
+        int cntR = pre[posR];
 
+        return L->cnt(cntL + 1, cntR, k) + R->cnt(posL - cntL, posR - cntR, k);
     }
 
     int Kth(int posL, int posR, int k) {
-
         if (l == r)
             return l;
-        
-        int cntL = prefix[posL - 1];
-        int cntR = prefix[posR];
+
+        int cntL = pre[posL - 1];
+        int cntR = pre[posR];
         int quantity = cntR - cntL;
 
         if (quantity >= k)
-            return childL->Kth(cntL + 1, cntR, k);
+            return L->Kth(cntL + 1, cntR, k);
         else
-            return childR->Kth(posL - cntL, posR - cntR, k - quantity);
-
+            return R->Kth(posL - cntL, posR - cntR, k - quantity);
     }
 } wave;
