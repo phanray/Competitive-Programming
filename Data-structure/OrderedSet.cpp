2e54e666c0a15f8f5afb10b273de92b291263ffb
Phan Thanh Đạt <datp3877@gmail.com>
2025-01-21 14:20:11 +0700

    Update OrderedSet.cpp

--- Data-structure/OrderedSet.cpp
+++ Data-structure/OrderedSet.cpp
@@ -6,3 +6,6 @@ using namespace __gnu_pbds;
 template<class T>
 using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,
     tree_order_statistics_node_update>;
+
+// order_of_key (k) : Number of items strictly smaller than k .
+// find_by_order(k) : K-th element in a set (counting from zero).
