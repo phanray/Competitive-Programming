7039b6092292b0bca9d2ff74aa1d4b9a29aa6261
Phan Thanh Đạt <datp3877@gmail.com>
2025-04-25 01:32:18 +0700

    Update Matrix.cpp

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -1,49 +1,47 @@
-using type = ll;
+struct Matrix{
+    vector<vector<ll> >a;
 
-struct Matrix {
-    vector<vector<type>>data;
-    int row() const{return data.size();} 
-    int col() const{return data[0].size();}
+    Matrix(int n, int m): a(n, vector<ll> (m)){}
 
-    auto & operator [] (int i) {return data[i];}
-    const auto & operator[] (int i) const {return data[i];}
+    Matrix(const vector<vector<ll> >&b): a(b){}
 
-    Matrix()=default;
-    Matrix(int r,int c):data(r,vector<type>(c)) {}
-    Matrix(const vector<vector<type>>&d):data(d) {}
+    int row() const {return a.size();}
 
-    friend ostream & operator << (ostream &out,const Matrix &d) {
-        for (auto x:d.data){
-            for (auto y:x) out<<y<<' ';
-            out<<'\n';
-        }
-        return out;
-    }
+    int col() const {return a[0].size();}
 
-    static Matrix identity(long long n){
-        Matrix a=Matrix(n,n);
-        while (n--)a[n][n]=1;
+    auto & operator [] (int i) {return a[i];}
+
+    const auto & operator[] (int i) const {return a[i];}
+
+    Matrix identity(ll n){
+        Matrix a = Matrix(n, n);
+
+        while (n--) 
+            a[n][n] = 1;
+
         return a;
     }
 
-    Matrix operator * (const Matrix &b){
-        Matrix a=*this;
-        assert(a.col()==b.row()); 
-        Matrix c(a.row(),b.col());
-        for (int i=0;i<a.row();++i)
-            for (int j=0;j<b.col();++j)
-                for (int k=0;k<a.col();++k){
-                    c[i][j]+=1ll*a[i][k]%mod*(b[k][j]%mod)%mod;
-                    c[i][j]%=mod;
-                }
+    Matrix operator * (const Matrix &b) {
+        assert(col() == b.row());
+        Matrix c(row(), b.col());
+
+        for (int i = 0; i < row(); ++i)
+            for (int j = 0; j < b.col(); ++j)
+                for (int k = 0; k < col(); ++k)
+                    c[i][j] += a[i][k] * b[k][j];
+
         return c;
     }
 
-    Matrix pow(long long exp){
-        assert(row()==col());  
-        Matrix base=*this,ans=identity(row());
-        for (;exp>0;exp>>=1,base=base*base)
-            if (exp&1) ans=ans*base;
+    Matrix pow(ll x){
+        assert(row() == col());
+        Matrix base = *this, ans = identity(row());
+
+        for (; x > 0; x >>= 1, base = base * base)
+            if (x & 1) 
+                ans = ans * base;
+
         return ans;
     }
 };
